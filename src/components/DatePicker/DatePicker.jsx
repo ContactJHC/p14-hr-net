c67eb30e868784a60e79cfa3d57418ade2e9daf2
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-02-01 14:46:31 +0100

    component datepicker

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function BasicTimePicker() {
+  const [value, setValue] = useState(null)
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <DatePicker
+        label="Basic example"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  )
+}
+
+
+// import { LocalizationProvider } from "@mui/x-date-pickers"
+// import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+// import { DatePicker } from "@mui/x-date-pickers"
+
+
+
+// export default function DatePicker() {
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterMoment}>
+//   <TimePicker
+//     label="Basic example"
+//     value={value}
+//     onChange={(newValue) => {
+//       setValue(newValue);
+//     }}
+//     renderInput={(params) => <TextField {...params} />}
+//   />
+//     </LocalizationProvider>
+//   )
+// }
--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Modal from '@contactjhc/modal-library'
-
+// import Modal from '@contactjhc/modal-library'
+import DatePicker from '../../components/DatePicker/DatePicker'
+// import { DatePicker } from "@mui/x-date-pickers"
 import EmployeesTable from '../../components/EmployeesTable/EmployeesTable'
 import SelectCountry from '../../components/SelectCountry/SelectCountry'
-// import Modal from '../../components/Modal/Modal'
+import Modal from '../../components/Modal/Modal'
 
 
 export default function Employees() {
@@ -19,7 +20,10 @@ export default function Employees() {
 
   return (
     <div id="employee-div" class="container">
+
         <h1>Current Employees</h1>
+        <DatePicker/>
+
         <table id="employee-table" class="display"></table>
         <Link to='/NewEmployee'>Create Employee</Link>
         <SelectCountry />
